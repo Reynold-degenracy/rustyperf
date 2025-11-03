b8dc3a9f5ce330f3ae2cd7c86f0806950423b2c1
Shinonome <reynold030512@gmail.com>
2025-11-03 23:28:00 +0800

    重构客户端逻辑，准备添加反向模式

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,19 @@
 use tokio::net::TcpStream;
-use tokio::io::AsyncReadExt;
+use tokio::io::{self, AsyncReadExt, AsyncWriteExt};
+use tokio::time::{Instant, Duration};
 
-pub async fn handle_connection(mut socket: TcpStream) {
+pub async fn handle_connection(mut socket: TcpStream) -> io::Result<u64> {
     let mut buffer = [0; 1024]; // 创建一个缓冲区
     // let mut total_bytes_received: usize = 0;
     loop {
-        // 4. 从接口读取数据
+        // 从接口读取数据
         // `read` 返回 0 表示连接已关闭
         let _n = match socket.read(&mut buffer).await {
-            Ok(n) if n == 0 => return,
+            Ok(n) if n == 0 => return Ok(0),
             Ok(n) => n,
             Err(e) => {
                 eprintln!("读取数据失败: {}", e);
-                return;
+                return Ok(0);
             }
         };
 
@@ -28,3 +29,36 @@ pub async fn handle_connection(mut socket: TcpStream) {
     }
 }
 
+pub async fn make_connection(mut stream: TcpStream, time: u64) -> io::Result<u64> {
+    let data = [0u8; 1024 * 64]; // 64KiB 的数据块
+    let start_time = Instant::now();
+    let test_duration: Duration = Duration::from_secs(time as u64);
+    let mut total_bytes_sent: u64 = 0;
+    let mut last_report_time = Instant::now();
+    let mut bytes_since_last_report: u64 = 0;
+    println!("开始{}秒测试...", time);
+    while Instant::now() - start_time < test_duration {
+        stream.write_all(&data).await?;
+        let bytes_sent = data.len() as u64;
+        total_bytes_sent += bytes_sent;
+        bytes_since_last_report += bytes_sent;
+        if last_report_time.elapsed() >= Duration::from_secs(1) {
+            let throughput_mbps = (bytes_since_last_report * 8) as f64 / 1_000_000.0;
+            println!("当前速度 {:.2} Mbps", throughput_mbps);
+
+            // 重置计数
+            bytes_since_last_report = 0;
+            last_report_time = Instant::now();
+        }
+    }
+
+    // 3. 计算并打印结果
+    let elapsed = start_time.elapsed();
+    let throughput_mbps = (total_bytes_sent as f64 * 8.0) / (elapsed.as_secs_f64() * 1_000_000.0);
+
+    println!("测试完成！");
+    println!("持续时间: {:.2?}", elapsed);
+    println!("总共发送: {:.2} MB", total_bytes_sent as f64 / 1_000_000.0);
+    println!("吞吐率: {:.2} Mbps", throughput_mbps);
+    Ok(0)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use tokio::net::TcpListener;
-use rustyperf::handle_connection;
+use tokio::net::{TcpStream, TcpListener};
+use rustyperf::{handle_connection, make_connection};
 use clap::Parser;
 
 
@@ -27,6 +27,8 @@ struct Config {
     #[arg(short='a', long="address", default_value_t = String::from("127.0.0.1"))]
     address: String,
 
+    #[arg(short='r', long="reverse", default_value_t = false)]
+    reverse: bool,
 }
 
 #[tokio::main]
@@ -35,65 +37,22 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let arg = Config::parse();
 
     if arg.client {
-
-        use tokio::net::TcpStream;
-        use tokio::io::AsyncWriteExt;
-        use tokio::time::{Instant, Duration};
-
         // 1. 连接到服务器
-        let mut stream = TcpStream::connect(format!("{}:{}", arg.address, arg.port)).await?;
+        let stream = TcpStream::connect(format!("{}:{}", arg.address, arg.port)).await?;
         println!("已连接到服务器");
-
-        let data = [0u8; 1024 * 64]; // 64KiB 的数据块
-        let test_duration: Duration = Duration::from_secs(arg.time as u64);
-        let start_time: Instant = Instant::now();
-        let mut total_bytes_sent: u64 = 0;
-
-        //添加实时报告功能
-        let mut last_report_time = Instant::now();
-        let mut bytes_since_last_report: u64 = 0;
-        println!("开始{}秒测试...", arg.time);
-
-        // 2. 在指定时间内持续发送数据
-        while Instant::now() - start_time < test_duration {
-            stream.write_all(&data).await?;
-            let bytes_sent = data.len() as u64;
-            total_bytes_sent += bytes_sent;
-            bytes_since_last_report += bytes_sent;
-            if last_report_time.elapsed() >= Duration::from_secs(1) {
-                let throughput_mbps = (bytes_since_last_report * 8) as f64 / 1_000_000.0;
-                println!("当前速度 {:.2} Mbps", throughput_mbps);
-
-                // Reset for the next interval
-                bytes_since_last_report = 0;
-                last_report_time = Instant::now();
-            }
-        }
-
-        // 3. 计算并打印结果
-        let elapsed = start_time.elapsed();
-        let throughput_mbps = (total_bytes_sent as f64 * 8.0) / (elapsed.as_secs_f64() * 1_000_000.0);
-
-        println!("测试完成！");
-        println!("持续时间: {:.2?}", elapsed);
-        println!("总共发送: {:.2} MB", total_bytes_sent as f64 / 1_000_000.0);
-        println!("吞吐率: {:.2} Mbps", throughput_mbps);
-
+        make_connection(stream, arg.time).await.unwrap();
     }
 
     else if arg.server {
-
         // 1. 创建一个 TCP 监听器并绑定到地址
         let listener = TcpListener::bind(format!("{}:{}", arg.address, arg.port)).await?;
         println!("服务器正在监听 {}:{}", arg.address, arg.port);
-        
         loop {
             // 2. 接受新的连接
-            let (socket, _) = listener.accept().await?;
-            
+            let (socket, _) = listener.accept().await?; 
             // 3. 为每个连接生成一个新任务，防止阻塞主循环
             tokio::spawn(async move {
-                handle_connection(socket).await;
+                handle_connection(socket).await.unwrap();
             });
         }
     }
